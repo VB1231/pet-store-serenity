e2f82951e972aee4c6473630f2363b4817610907
vb1231 <viral2627@gmail.com>
2023-01-03 14:19:01 +0000

    pet-store-serenity

---
+++ src/main/java/com/petstore/constants/EndPoints.java
@@ -0,0 +1,34 @@
+package com.petstore.constants;
+
+/**
+ * Created by Jay
+ */
+public class EndPoints {
+
+    /**
+     * This is Endpoints of PetStore api
+     */
+    public static final String GET_ALL_PET = "/pet";
+
+    public static final String GET_SINGLE_USER_BY_ID = "/{id}";
+
+    public static final String UPDATE_USER_BY_ID="/{id}";
+
+    public static final String DELETE_USER_BY_ID = "/{id}";
+
+    /**
+     * This is Endpoints of Authentication api
+     */
+    /**
+     * This is Endpoints of USer api
+     */
+    public static final String GET_ALL_USERS = "/user";
+
+    public static final String GET_SINGLE_USER_BY_NAME = "/{name}";
+
+    public static final String UPDATE_USER_BY_NAME="/{name}";
+
+    public static final String DELETE_USER_BY_NAME = "/{name}";
+
+
+}
---
+++ src/main/java/com/petstore/constants/Path.java
@@ -0,0 +1,20 @@
+package com.petstore.constants;
+
+/**
+ * Created by Jay
+ */
+public class Path {
+    /*
+     * This is base path for pet store api
+     */
+    public static final String PET = "/pet";
+    public static final String USER = "/user";
+    /*
+     * This is base path for authenticate api
+     */
+    /*public static final String AUTHENTICATE = "/authenticate";
+
+    public static final String ACCOUNT = "/account";*/
+
+
+}
